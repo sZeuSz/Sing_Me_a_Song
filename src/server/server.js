b244e7f55c33ccfaa02209046e4a27ea5cbe5389
Bruna Topan <brutopanb@gmail.com>
2021-12-09 09:11:21 -0300

    Preparando review

--- src/server/server.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-import '../setup/setup.js';
-import app from '../app.js';
-
-app.listen(process.env.PORT, () => {
-  // console.log(`running in port ${process.env.PORT}`);
-});
